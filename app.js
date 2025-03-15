5e9f245b8b6a436350e1f2158828d0ab12f5b942
praful-koli <prafulkoli852@gmail.com>
2025-03-15 15:28:33 +0530

    update code

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.put("/listings/:id", async (req, res) => {
 // Delete Route
 
 app.delete("/listings/:id", async (req, res) => {
-  let { id } = req.params;
+   let { id } = req.params;
   await Listing.findByIdAndDelete(id);
   res.redirect("/listings");
 });
