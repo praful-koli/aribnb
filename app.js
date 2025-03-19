a0dc3958e038f06efe7e689afcf090d98fb040d6
praful-koli <prafulkoli852@gmail.com>
2025-03-19 18:31:22 +0530

    update code

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.get("/listings/:id/edit", async (req, res) => {
 
 // Update route
 
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", async (req, res) =>  {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect("/listings");
